ea4ff7ad422b432216590aa5ec8cc8c7133b36c9
Chandrasekar <chandrasekar51200@gmail.com>
2023-12-15 09:44:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,22 @@
 let height = document.getElementById("height");
 let weight = document.getElementById("weight");
 let btnSubmit = document.querySelector(".btnSubmit");
+let outerSection = document.querySelector(".outerBox");
+let modal = document.querySelector(".modalWindow");
+let displayBmi = document.querySelector(".bmiDisplay");
+let exitBtn = document.querySelector(".exitButton");
 
 
 let bmi;
 btnSubmit.addEventListener("click", function() {
-    bmi = (+weight.value /  Number((height.value ** 2))) * 10000;
-    document.querySelector(".result").textContent = `Your BMI value is ${bmi}`;
-    // console.log(bmi);
+    // document.querySelector(".result").textContent = `Your BMI value is ${bmi}`;ss
+    outerSection.classList.toggle("hidden");
+    modal.classList.toggle("hidden");
+    displayBmi.textContent = `Your BMI is ${bmi}`;
+});
+
+exitBtn.addEventListener("click", function() {
+    bmi = ((+weight.value /  Number((height.value ** 2))) * 10000).toFixed(1);
+    outerSection.classList.toggle("hidden");
+    modal.classList.toggle("hidden");
 });
